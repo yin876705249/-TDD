5f28527cbfe115f387d38a17a2177d1132e60484
yin876705249 <876705249@qq.com>
2021-05-21 14:08:09 +0800

    PPT7 page51

--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,13 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
+    return render(request, 'home.html')
+    """
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
     return render(request,'home.html')
+    """
     #items = Item.objects.all()
     #return render(request, 'home.html',{'items':items})
 
@@ -15,4 +18,7 @@ def view_list(request):
     items = Item.objects.all()
     return render(request,'list.html',{'items':items})
 
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
